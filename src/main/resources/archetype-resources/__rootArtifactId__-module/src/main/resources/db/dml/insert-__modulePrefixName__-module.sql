bf0d36ead3a03195009310143f2b7d4f5a457694
Danilo Mesquita <danilofmesquita@gmail.com>
2017-02-14 16:57:10 -0200

    atualizando para https

--- src/main/resources/archetype-resources/__rootArtifactId__-module/src/main/resources/db/dml/insert-__modulePrefixName__-module.sql
+++ src/main/resources/archetype-resources/__rootArtifactId__-module/src/main/resources/db/dml/insert-__modulePrefixName__-module.sql
@@ -3,5 +3,5 @@ INSERT INTO DBSINGULAR.TB_GRUPO_PROCESSO VALUES
 (
 '${modulePrefixName.toUpperCase()}MODULE',
 '${modulePrefixName.toUpperCase()}MODULE',
-'http://localhost:8080/${modulePrefixName}module/rest/flow'
+'https://localhost:8443/${modulePrefixName}module/rest/flow'
 );
