c8a9d8a8f38293b719684fa283e093821f98d6ef
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-04-02 09:57:57 -0300

    [SGL_616] - Adequação do Pom para utilizar o bom, e inclusão do FLowRenderTest

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-app/src/test/java/__modulePrefixName__module/flow/__moduleClassNamePrefix__FlowRenderTest.java
@@ -0,0 +1,23 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${modulePrefixName}module.flow;
+
+import ${package}.${modulePrefixName}module.flow.${moduleClassNamePrefix}Flow;
+import org.junit.Test;
+import org.opensingular.requirement.commons.test.flow.AbstractFlowRenderTest;
+
+public class ${moduleClassNamePrefix}FlowRenderTest extends AbstractFlowRenderTest {
+
+    public ${moduleClassNamePrefix}FlowRenderTest() {
+        setOpenGeneratedFiles(true);
+    }
+
+    @Test
+    public void render() {
+        super.renderImage(new ${moduleClassNamePrefix}Flow());
+    }
+}
+
+
+
